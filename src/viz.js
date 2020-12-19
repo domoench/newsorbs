dfb2471266a81ba2a7358d22d479981a196ec857
David Moench <domoench@gmail.com>
2020-12-18 21:26:59 -0500

    Animate object scale based on frequency data.

--- src/viz.js
+++ src/viz.js
@@ -14,7 +14,7 @@ const Viz = () => {
     const listener = audioContext.listener;
     listener.positionX.value = 0;
     listener.positionY.value = 0;
-    listener.positionZ.value = 0;
+    listener.positionZ.value = 2;
     listener.forwardX.value = 0;
     listener.forwardY.value = 0;
     listener.forwardZ.value = -1;
@@ -52,14 +52,24 @@ const Viz = () => {
       rolloffFactor: rollOff,
     });
 
+    // Analysis
+    const analyser = audioContext.createAnalyser();
+    analyser.smoothingTimeConstant = 0.8;
+    analyser.fftSize = 256;
+    const bufferLength = analyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+    console.log('dataArray', dataArray);
+
     // Connnect our audio graph
-    track.connect(orbPannerNode).connect(audioContext.destination);
+    track
+      .connect(analyser)
+      .connect(orbPannerNode)
+      .connect(audioContext.destination);
 
     document.querySelector('button#continue').addEventListener('click', () => {
       audioContext.resume().then(async () => {
         console.log('Playback resumed successfully');
         audioElement.play();
-
       });
     });
 
@@ -68,14 +78,22 @@ const Viz = () => {
     // buildRenderer: https://github.com/PierfrancescoSoffritti/pierfrancescosoffritti.com/blob/master/src/components/home/header/threejs/SceneManager.js#L32
     const canvas = canvasRef.current;
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshPhongMaterial({color: '#F00'});
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
+
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 10, 0);
+    light.target.position.set(-5, 0, 0);
+    scene.add(light);
+    scene.add(light.target);
 
     camera.position.z = 5;
 
@@ -85,14 +103,21 @@ const Viz = () => {
       if (orbPannerNode.positionX.value >= orbMaxX) {
         orbPosition.X *= -1;
       }
-      orbPosition.X += 0.1;
+      orbPosition.X += 0.02;
       orbPannerNode.positionX.value = orbPosition.X;
-      console.log(`Orb Position: x:${orbPannerNode.positionX.value}. y:${orbPannerNode.positionY.value}.`);
 
       cube.position.x = orbPosition.X;
 
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
+
+      // Scale the cube based on real time frequency data
+      analyser.getByteFrequencyData(dataArray);
+      const [low, mid, high] = [dataArray[0], dataArray[20], dataArray[60]];
+      const scale = (x) => (x / 200) * 3.0 + 0.5;
+      cube.scale.x = scale(low);
+      cube.scale.y = scale(mid);
+      cube.scale.z = scale(high);
 
       renderer.render( scene, camera );
     };
@@ -108,5 +133,6 @@ const Viz = () => {
     </>
   );
 };
+// https://emp.bbci.co.uk/emp/media/blank.mp3 // TODO BBC
 
 export default Viz;
