552fcfab314b9e785b90db39161e80a9270cb8db
David Moench <domoench@gmail.com>
2020-12-18 19:58:34 -0500

    Basic three.js visualization.
    Real messy. Move the sound and a 3D object in sync.

--- src/viz.js
+++ src/viz.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 const Viz = () => {
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     const audioContext = new AudioContext();
@@ -21,15 +23,16 @@ const Viz = () => {
     listener.upZ.value = 0;
 
     // Sound source position in space
+    const orbMaxX = 15;
     const pannerModel = 'HRTF';
-    const distanceModel = 'inverse';
+    const distanceModel = 'exponential';
     const maxDistance = 100;
     const refDistance = 1;
     const rollOff = 1;
     const orbPosition = {
-      positionX: -40,
-      positionY: 0,
-      positionZ: 0,
+      X: -1 * orbMaxX,
+      Y: 0,
+      Z: 0,
       orientationX: 0,
       orientationY: 0,
       orientationZ: -1,
@@ -38,9 +41,9 @@ const Viz = () => {
     const orbPannerNode = new PannerNode(audioContext, {
       panningModel: pannerModel,
       distanceModel: distanceModel,
-      positionX: orbPosition.positionX,
-      positionY: orbPosition.positionY,
-      positionZ: orbPosition.positionZ,
+      positionX: orbPosition.X,
+      positionY: orbPosition.Y,
+      positionZ: orbPosition.Z,
       orientationX: orbPosition.orientationX,
       orientationY: orbPosition.orientationY,
       orientationZ: orbPosition.orientationZ,
@@ -57,25 +60,51 @@ const Viz = () => {
         console.log('Playback resumed successfully');
         audioElement.play();
 
-        while (orbPannerNode.positionX.value < 40) {
-          orbPannerNode.positionX.value += 0.1;
-          await new Promise(r => setTimeout(r, 10));
-          console.log(`Orb Position: x:${orbPannerNode.positionX.value}. y:${orbPannerNode.positionY.value}.`);
-        }
       });
     });
 
-    document.querySelector('button#right').addEventListener('click', function() {
-      orbPannerNode.positionX.value += 0.5;
+    // VISUALS
+
+    // buildRenderer: https://github.com/PierfrancescoSoffritti/pierfrancescosoffritti.com/blob/master/src/components/home/header/threejs/SceneManager.js#L32
+    const canvas = canvasRef.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      if (orbPannerNode.positionX.value >= orbMaxX) {
+        orbPosition.X *= -1;
+      }
+      orbPosition.X += 0.1;
+      orbPannerNode.positionX.value = orbPosition.X;
       console.log(`Orb Position: x:${orbPannerNode.positionX.value}. y:${orbPannerNode.positionY.value}.`);
-    });
+
+      cube.position.x = orbPosition.X;
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+
+    animate();
   });
 
   return (
     <>
       <audio src="https://npr-ice.streamguys1.com/live.mp3?ck=1608315112307" crossOrigin="anonymous"></audio>
       <button type="button" id="continue">Continue</button>
-      <button type="button" id="right">Right</button>
+      <canvas ref={canvasRef}></canvas>
     </>
   );
 };
