01f1d956481e5ab933abe2e4e7e9281aef27f550
David Moench <domoench@gmail.com>
2020-12-26 12:41:12 -0500

    Encapsulate orb logic in Orb class.
    - Use that to create multiple orb instances
    - Implement poor-mans velocity and orb motion towards random
      destinations in the 3D space.

---
+++ src/Orb.js
@@ -0,0 +1,210 @@
+import * as THREE from "three";
+
+// State:
+//   - Stream URL
+//   - Position (x,y,z)
+//   - Destination (x, y, z)
+//   - Mass, Velocity, Acceleration?
+//
+//   - Color base
+//   - group <THREE.Group>
+//
+//  Methods
+//   - constructor: Initializes the group (inits Geometry, adds multiple transforme Points, add to scene)
+//   - animate: Updates orb position, and three.js stuff (bufferGeometry attributes,
+//     translates + rotates group)
+//   - setDestination
+
+// Orb Class
+// Manages the following
+// - Orb 'physical' properties: position, destination, etc
+// - Orb's WebAudio audio state
+// - Orb's Three.js state
+// Mainly to update them all in sync on each animation tick.
+
+// TODO move some of these to a constants/config file
+const room = {
+  minX: -60,
+  maxX: 60,
+  minY: -60,
+  maxY: 60,
+  minZ: -60,
+  maxZ: 60,
+};
+const numParticles = 100;
+const particleSize = 0.025;
+
+export default class Orb {
+  // params object looks like:
+  // {
+  //   position: <THREE.Vector3>
+  //   audioSelector: <String>
+  //   audioContext: <WebAudioAPI AudioContext instance>
+  //   baseColor: <hexidecimal>
+  // }
+  // TODO: Typescript?
+  constructor(params) {
+    // 'PHYSICAL' STATE
+    this.position = params.position;
+
+    this.destination = new THREE.Vector3();
+    this.setRandomDestination();
+
+    // THREE.JS STATE
+    // Initialize BufferGeometry and Points
+    this.bufferGeom = new THREE.BufferGeometry();
+    const positions = [];
+    const colors = [];
+    const sizes = [];
+    const color = new THREE.Color(params.baseColor);
+    for (let i = 0; i < numParticles; i++) {
+      const x = i * particleSize;
+      const y = 0;
+      const z = 0;
+      positions.push(x, y, z);
+      colors.push(color.r, color.g, color.b);
+      sizes.push(0.001);
+    }
+    this.bufferGeom.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    this.bufferGeom.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    this.bufferGeom.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
+    this.pointsMaterial = new THREE.PointsMaterial( { size: particleSize, vertexColors: true } );
+
+    this.group = new THREE.Group();
+    const numHairs = 720;
+    const numSlices = 48;
+    const numHairsPerSlice = numHairs / numSlices;
+    for (let i = 0; i < numSlices; i++) {
+      // Slices rotate around the y-axis
+      const yRotDeg = (360.0 / numSlices) * i;
+      for (let j = 0; j < numHairsPerSlice; j++) {
+        const points = new THREE.Points(this.bufferGeom, this.pointsMaterial);
+        const zRotDeg = (360.0 / numHairsPerSlice) * j;
+        points.rotation.y = THREE.MathUtils.degToRad(yRotDeg);
+        points.rotation.z = THREE.MathUtils.degToRad(zRotDeg);
+        this.group.add(points);
+      }
+    }
+
+    // WEBAUDIO API STATE
+    this.audioElement = document.querySelector(params.audioSelector);
+    const track = params.audioContext.createMediaElementSource(this.audioElement);
+
+    // Orb's sound position in space
+    const pannerModel = 'HRTF';
+    const distanceModel = 'exponential';
+    const maxDistance = 100;
+    const refDistance = 1;
+    const rollOff = 1.5;
+    const orbOrientation = { x: 0, y: 0, z: -1 };
+    this.pannerNode = new PannerNode(params.audioContext, {
+      panningModel: pannerModel,
+      distanceModel: distanceModel,
+      positionX: this.position.x,
+      positionY: this.position.y,
+      positionZ: this.position.z,
+      orientationX: orbOrientation.x,
+      orientationY: orbOrientation.y,
+      orientationZ: orbOrientation.z,
+      refDistance: refDistance,
+      maxDistance: maxDistance,
+      rolloffFactor: rollOff,
+    });
+
+    // Analysis
+    this.analyser = params.audioContext.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.8;
+    this.analyser.fftSize = 256;
+    const bufferLength = this.analyser.frequencyBinCount;
+    this.freqDataArray = new Uint8Array(bufferLength);
+
+    // Connnect our audio graph
+    track
+      .connect(this.analyser)
+      .connect(this.pannerNode)
+      .connect(params.audioContext.destination);
+  }
+
+  // Add this orb's THREE.Group to the given scene
+  addToScene(scene) {
+    scene.add(this.group);
+  }
+
+  // Set a new random destination within the space
+  setRandomDestination() {
+    const rangeX = room.maxX - room.minX;
+    const rangeY = room.maxY - room.minY;
+    const rangeZ = room.maxZ - room.minZ;
+    this.destination.x = (Math.random() * rangeX) - rangeX / 2; // TODO util function randInRange
+    this.destination.y = (Math.random() * rangeY) - rangeY / 2;
+    this.destination.z = (Math.random() * rangeZ) - rangeZ / 2;
+    console.log('randomDestination', this.destination);
+  }
+
+  // Update the orbs properties for 1 animation tick
+  animate() { // TODO dt arg?
+    // Update the Orb's physical properties
+    // Position. Move sort-of towards the destination
+    // For now do a simple/hacky constant component velocity of 0.1 for all x,y,z
+    // TODO velocity, acceleration equations more like real motion.
+    const xDist = this.destination.x - this.position.x;
+    const xDir = xDist / Math.abs(xDist);
+    const yDist = this.destination.y - this.position.y;
+    const yDir = yDist / Math.abs(yDist);
+    const zDist = this.destination.z - this.position.z;
+    const zDir = zDist / Math.abs(zDist);
+    this.position.x += 0.1 * xDir;
+    this.position.y += 0.1 * xDir;
+    this.position.z += 0.1 * xDir;
+    // TODO: Another hack shortcut to fix later
+    // If we are 'close' to our destination, set a new one
+    if (Math.abs(this.position.x - this.destination.x) < 0.2) {
+      this.setRandomDestination();
+    }
+
+    // Update sound position
+    this.pannerNode.positionX.value = this.position.x;
+
+    // Update the BufferGeometry
+    this.analyser.getByteFrequencyData(this.freqDataArray);
+    const [low, mid, high] = [this.freqDataArray[0], this.freqDataArray[20], this.freqDataArray[60]];
+
+    // Animate the particles
+    const positions = this.bufferGeom.attributes.position.array;
+
+    // 1. Shift everything right
+    // TODO: This is conceptually easier, but not efficient. Should
+    // do a circular buffer where we track end position, and then just
+    // do +1 to x coord (no need to shift verticies in the array)
+    let prevPoint = [positions[0], positions[1], positions[2]];
+    let currPoint = [0,0,0];
+    for (let particleIdx = 1; particleIdx < numParticles; particleIdx++) {
+      const i = particleIdx * 3;
+      currPoint = [positions[i+0], positions[i+1], positions[i+2]];
+
+      // Overwrite this point
+      positions[i + 0] = prevPoint[0] + particleSize; // x (shifted right)
+      positions[i + 1] = prevPoint[1] * 1.001; // y with expansion
+      positions[i + 2] = prevPoint[2] * 0.999; // z with contraction
+
+      // Save for next iteration overwrite
+      prevPoint[0] = currPoint[0]; // x
+      prevPoint[1] = currPoint[1]; // y
+      prevPoint[2] = currPoint[2]; // z
+    }
+    // 2. Set new first point Y position
+    positions[0] = low * 0.005;
+    positions[1] = mid * 0.005;
+    positions[2] = high * 0.005;
+    this.bufferGeom.attributes.position.needsUpdate = true;
+    // TODO: Perhaps need to recompute boundingBox or Sphere? https://threejs.org/docs/#manual/en/introduction/How-to-update-things
+
+    // Update the Group
+    this.group.position.x = this.position.x;
+    this.group.position.y = this.position.y;
+    this.group.position.z = this.position.z;
+    this.group.rotation.x += 0.005;
+    this.group.rotation.y += 0.01;
+
+  }
+}
--- src/viz.js
+++ src/viz.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 
+import Orb from "./Orb";
+
 const Viz = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const audioContext = new AudioContext();
-    const audioElement = document.querySelector('audio');
-    const track = audioContext.createMediaElementSource(audioElement);
 
     // Spatialization
     // Listener is our position in space
     const listener = audioContext.listener;
     listener.positionX.value = 0; // TODO this doesn't work in firefox: https://developer.mozilla.org/en-US/docs/Web/API/AudioListener#Methods
     listener.positionY.value = 0;
-    listener.positionZ.value = 2;
+    listener.positionZ.value = 30;
     listener.forwardX.value = 0;
     listener.forwardY.value = 0;
     listener.forwardZ.value = -1;
@@ -22,53 +22,24 @@ const Viz = () => {
     listener.upY.value = 1;
     listener.upZ.value = 0;
 
-    // Sound source position in space
-    const orbMaxX = 15;
-    const pannerModel = 'HRTF';
-    const distanceModel = 'exponential';
-    const maxDistance = 100;
-    const refDistance = 1;
-    const rollOff = 1;
-    const orbPosition = {
-      X: -1 * orbMaxX,
-      Y: 0,
-      Z: 0,
-      orientationX: 0,
-      orientationY: 0,
-      orientationZ: -1,
-    };
-
-    const orbPannerNode = new PannerNode(audioContext, {
-      panningModel: pannerModel,
-      distanceModel: distanceModel,
-      positionX: orbPosition.X,
-      positionY: orbPosition.Y,
-      positionZ: orbPosition.Z,
-      orientationX: orbPosition.orientationX,
-      orientationY: orbPosition.orientationY,
-      orientationZ: orbPosition.orientationZ,
-      refDistance: refDistance,
-      maxDistance: maxDistance,
-      rolloffFactor: rollOff,
+    const orb = new Orb({
+      position: new THREE.Vector3(-15, 0, 0),
+      audioSelector: 'audio#npr',
+      audioContext: audioContext,
+      baseColor: 0xff0000,
     });
-
-    // Analysis
-    const analyser = audioContext.createAnalyser();
-    analyser.smoothingTimeConstant = 0.8;
-    analyser.fftSize = 256;
-    const bufferLength = analyser.frequencyBinCount;
-    let freqDataArray = new Uint8Array(bufferLength);
-
-    // Connnect our audio graph
-    track
-      .connect(analyser)
-      .connect(orbPannerNode)
-      .connect(audioContext.destination);
+    const orb2 = new Orb({
+      position: new THREE.Vector3(10, 0, 0),
+      audioSelector: 'audio#random',
+      audioContext: audioContext,
+      baseColor: 0x00ff00,
+    });
 
     document.querySelector('button#continue').addEventListener('click', () => {
       audioContext.resume().then(async () => {
         console.log('Playback resumed successfully');
-        audioElement.play();
+        orb.audioElement.play();
+        orb2.audioElement.play();
       });
     });
 
@@ -77,7 +48,7 @@ const Viz = () => {
     // buildRenderer: https://github.com/PierfrancescoSoffritti/pierfrancescosoffritti.com/blob/master/src/components/home/header/threejs/SceneManager.js#L32
     const canvas = canvasRef.current;
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 10;
+    camera.position.z = 30; // TODO need to keep camera position and listener position in sync
 
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -85,111 +56,14 @@ const Viz = () => {
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
 
-    // Lights
-    const intensity = 1;
-    const redLight = new THREE.DirectionalLight(0xFF0000, intensity);
-    redLight.position.set(5, 10, -5);
-    redLight.target.position.set(0, 0, 0);
-    scene.add(redLight);
-    scene.add(redLight.target);
-    const blueLight = new THREE.DirectionalLight(0x0000FF, intensity);
-    blueLight.position.set(-5, -10, -2);
-    blueLight.target.position.set(0, 0, 0);
-    scene.add(blueLight);
-    scene.add(blueLight.target);
-
-    // Points
-    const numParticles = 100;
-    const bufferGeom = new THREE.BufferGeometry();
-    const positions = [];
-    const colors = [];
-    const sizes = [];
-    const color = new THREE.Color();
-    const particleSize = 0.025;
-    color.setRGB(1.0, 0, 0);
-    for (let i = 0; i < numParticles; i++) {
-      const x = i * particleSize;
-      const y = 0;
-      const z = 0;
-      positions.push(x, y, z);
-      colors.push(color.r, color.g, color.b);
-      sizes.push(0.001);
-    }
-    bufferGeom.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-    bufferGeom.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-    bufferGeom.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
-    const pointsMaterial = new THREE.PointsMaterial( { size: particleSize, vertexColors: true } );
-
-    const orbGroup = new THREE.Group();
-    const numHairs = 720;
-    const numSlices = 48;
-    const numHairsPerSlice = numHairs / numSlices;
-    for (let i = 0; i < numSlices; i++) {
-      // Slices rotate around the y-axis
-      const yRotDeg = (360.0 / numSlices) * i;
-      for (let j = 0; j < numHairsPerSlice; j++) {
-        const points = new THREE.Points( bufferGeom, pointsMaterial );
-        const zRotDeg = (360.0 / numHairsPerSlice) * j;
-        points.rotation.y = THREE.MathUtils.degToRad(yRotDeg);
-        points.rotation.z = THREE.MathUtils.degToRad(zRotDeg);
-        orbGroup.add(points);
-      }
-    }
-    scene.add(orbGroup);
-
-    const animateBufferGeom = (bufferGeom, freqDataArray) => {
-      const [low, mid, high] = [freqDataArray[0], freqDataArray[20], freqDataArray[60]];
-
-      // Animate the particles
-      const positions = bufferGeom.attributes.position.array;
-
-      // 1. Shift everything right
-      // TODO: This is conceptually easier, but not efficient. Should
-      // do a circular buffer where we track end position, and then just
-      // do +1 to x coord (no need to shift verticies in the array)
-      let prevPoint = [positions[0], positions[1], positions[2]];
-      let currPoint = [0,0,0];
-      for (let particleIdx = 1; particleIdx < numParticles; particleIdx++) {
-        const i = particleIdx * 3;
-        currPoint = [positions[i+0], positions[i+1], positions[i+2]];
-
-        // Overwrite this point
-        positions[i + 0] = prevPoint[0] + particleSize; // x (shifted right)
-        positions[i + 1] = prevPoint[1] * 1.001; // y with expansion
-        positions[i + 2] = prevPoint[2] * 0.999; // z with contraction
-
-        // Save for next iteration overwrite
-        prevPoint[0] = currPoint[0]; // x
-        prevPoint[1] = currPoint[1]; // y
-        prevPoint[2] = currPoint[2]; // z
-      }
-      // 2. Set new first point Y position
-      positions[0] = low * 0.005;
-      positions[1] = mid * 0.005;
-      positions[2] = high * 0.005;
-      bufferGeom.attributes.position.needsUpdate = true;
-      // TODO: Perhaps need to recompute boundingBox or Sphere? https://threejs.org/docs/#manual/en/introduction/How-to-update-things
-    }
+    orb.addToScene(scene);
+    orb2.addToScene(scene);
 
     const animate = () => {
       requestAnimationFrame(animate);
 
-      if (orbPosition.X >= orbMaxX) {
-        orbPosition.X *= -1;
-      }
-      orbPosition.X += 0.05;
-      orbPannerNode.positionX.value = orbPosition.X;
-
-      // TODO is pointing the lights at the orb working?
-      redLight.target.position.set(orbPosition.X, orbPosition.Y, orbPosition.Z);
-      blueLight.target.position.set(orbPosition.X, orbPosition.Y, orbPosition.Z);
-
-      analyser.getByteFrequencyData(freqDataArray);
-      animateBufferGeom(bufferGeom, freqDataArray);
-
-      orbGroup.position.x = orbPosition.X;
-      orbGroup.rotation.x += 0.005;
-      orbGroup.rotation.y += 0.01;
+      orb.animate();
+      orb2.animate();
 
       renderer.render(scene, camera);
     };
@@ -199,12 +73,12 @@ const Viz = () => {
 
   return (
     <>
-      <audio src="https://npr-ice.streamguys1.com/live.mp3?ck=1608315112307" crossOrigin="anonymous"></audio>
+      <audio id="npr" src="https://npr-ice.streamguys1.com/live.mp3?ck=1608315112307" crossOrigin="anonymous"></audio>
+      <audio id="random" src="http://149.56.147.197:7000/stream" crossOrigin="anonymous"></audio>
       <button type="button" id="continue">Continue</button>
       <canvas ref={canvasRef}></canvas>
     </>
   );
 };
-// https://emp.bbci.co.uk/emp/media/blank.mp3 // TODO BBC
 
 export default Viz;
